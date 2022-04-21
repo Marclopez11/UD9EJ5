a38e21fee3fcef2b2fb95265333adebb2b15d054
Marc Lopez <marclopezmarco469@gmail.com>
2022-04-21 23:33:53 +0200

    Clases Profesor

--- UD9EJ5/src/ej5/Profesor.java
+++ UD9EJ5/src/ej5/Profesor.java
@@ -1,5 +1,35 @@
 package ej5;
 
-public class Profesor {
+import java.util.Arrays;
+
+public class Profesor extends Persona{
+	
+	 private String[] materias = { "Matemáticas", "Filosofía", "Física" };
+
+	public Profesor(String nom, int edad, String sexe, String[] materias) {
+		super(nom, edad, sexe);
+		this.materias = materias;
+	}
+
+	public String[] getMaterias() {
+		return materias;
+	}
+
+	public void setMaterias(String[] materias) {
+		this.materias = materias;
+	}
+
+	@Override
+	public String toString() {
+		return "Profesor [materias=" + Arrays.toString(materias) + "]";
+	}
+
+
+
+
+	 
+
+
+
 
 }
