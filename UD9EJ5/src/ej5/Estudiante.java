ebe181f7eef570a66bf0edf5ea51f4be84933fa9
Marc Lopez <marclopezmarco469@gmail.com>
2022-04-21 23:40:29 +0200

    Clases Estudiante

--- UD9EJ5/src/ej5/Estudiante.java
+++ UD9EJ5/src/ej5/Estudiante.java
@@ -1,5 +1,32 @@
 package ej5;
 
-public class Estudiante {
+public class Estudiante extends Persona{
+	
+	
+	private int calificacion;
+
+	public Estudiante(String nom, int edad, String sexe, int calificacion) {
+		super(nom, edad, sexe);
+		this.calificacion = calificacion;
+	}
+
+	public int getCalificacion() {
+		return calificacion;
+	}
+
+	public void setCalificacion(int calificacion) {
+		this.calificacion = calificacion;
+	}
+
+	@Override
+	public String toString() {
+		return "Estudiante [calificacion=" + calificacion + "]";
+	}
+
+	
+	
+	
+	
+	
 
 }
